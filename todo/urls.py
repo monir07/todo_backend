da08a823ed8291f5d53010f62062c11db34510b2
Monir <monirulslm7@gmail.com>
2024-04-04 16:22:41 +0600

    url name modified.

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,5 @@ from .views import (ListCreateTodo, Login, RetrieveUdateDestroyTodo )
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('list-create-todo/', ListCreateTodo.as_view(), name='list-create-todo'),
-    path('retrieve-update-destroy-todo/<int:pk>', RetrieveUdateDestroyTodo.as_view(), name='retrieve-update-destroy'),
+    path('retrieve-update-destroy-todo/<int:pk>/', RetrieveUdateDestroyTodo.as_view(), name='retrieve-update-destroy'),
 ]
